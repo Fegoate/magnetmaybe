697ed4f77b6257623eafe5a4ae72d9e30aea578b
Wotjak <149939128+Fegoate@users.noreply.github.com>
2025-12-27 21:16:25 +0800

    Align heatmap rendering with axes

--- java/src/main/java/rcs/DirectionHeatmapPanel.java
+++ java/src/main/java/rcs/DirectionHeatmapPanel.java
@@ -40,6 +40,17 @@ public class DirectionHeatmapPanel extends JPanel {
         int plotWidth = width - 2 * margin;
         int plotHeight = height - 2 * margin;
 
+        double azMin = azimuthsDeg[0];
+        double azMax = azimuthsDeg[azimuthsDeg.length - 1];
+        double elMin = elevationsDeg[0];
+        double elMax = elevationsDeg[elevationsDeg.length - 1];
+        if (azMax == azMin) {
+            azMax = azMin + 1.0;
+        }
+        if (elMax == elMin) {
+            elMax = elMin + 1.0;
+        }
+
         g2.setColor(Color.WHITE);
         g2.fillRect(0, 0, width, height);
         g2.setColor(Color.GRAY);
@@ -59,16 +70,23 @@ public class DirectionHeatmapPanel extends JPanel {
         }
         double scale = Math.max(1e-6, maxRcs - minRcs);
 
-        int cellW = Math.max(1, plotWidth / Math.max(1, azimuthsDeg.length - 1));
-        int cellH = Math.max(1, plotHeight / Math.max(1, elevationsDeg.length - 1));
+        double[] azBounds = computeBounds(azimuthsDeg);
+        double[] elBounds = computeBounds(elevationsDeg);
         for (int i = 0; i < elevationsDeg.length; i++) {
             for (int j = 0; j < azimuthsDeg.length; j++) {
                 double v = rcsValues[i][j];
                 float t = (float) ((v - minRcs) / scale);
                 Color c = new Color(t, 0.2f, 1.0f - t);
                 g2.setColor(c);
-                int x = margin + j * cellW;
-                int y = margin + (elevationsDeg.length - 1 - i) * cellH;
+
+                int x0 = valueToX(azBounds[j], azMin, azMax, margin, plotWidth);
+                int x1 = valueToX(azBounds[j + 1], azMin, azMax, margin, plotWidth);
+                int y0 = valueToY(elBounds[i], elMin, elMax, margin, plotHeight);
+                int y1 = valueToY(elBounds[i + 1], elMin, elMax, margin, plotHeight);
+                int x = Math.min(x0, x1);
+                int y = Math.min(y0, y1);
+                int cellW = Math.max(1, Math.abs(x1 - x0));
+                int cellH = Math.max(1, Math.abs(y1 - y0));
                 g2.fillRect(x, y, cellW, cellH);
             }
         }
@@ -77,20 +95,9 @@ public class DirectionHeatmapPanel extends JPanel {
         FontMetrics fm = g2.getFontMetrics();
         int tickCount = 4;
 
-        double azMin = azimuthsDeg[0];
-        double azMax = azimuthsDeg[azimuthsDeg.length - 1];
-        double elMin = elevationsDeg[0];
-        double elMax = elevationsDeg[elevationsDeg.length - 1];
-        if (azMax == azMin) {
-            azMax = azMin + 1.0;
-        }
-        if (elMax == elMin) {
-            elMax = elMin + 1.0;
-        }
-
         for (int i = 0; i <= tickCount; i++) {
             double azTick = azMin + (azMax - azMin) * i / tickCount;
-            int x = margin + (int) ((azTick - azMin) / (azMax - azMin) * plotWidth);
+            int x = valueToX(azTick, azMin, azMax, margin, plotWidth);
             g2.drawLine(x, margin + plotHeight, x, margin + plotHeight + 4);
             String label = formatTick(azTick);
             g2.drawString(label, x - fm.stringWidth(label) / 2, margin + plotHeight + fm.getAscent() + 6);
@@ -98,7 +105,7 @@ public class DirectionHeatmapPanel extends JPanel {
 
         for (int i = 0; i <= tickCount; i++) {
             double elTick = elMin + (elMax - elMin) * i / tickCount;
-            int y = margin + plotHeight - (int) ((elTick - elMin) / (elMax - elMin) * plotHeight);
+            int y = valueToY(elTick, elMin, elMax, margin, plotHeight);
             g2.drawLine(margin - 4, y, margin, y);
             String label = formatTick(elTick);
             g2.drawString(label, margin - 8 - fm.stringWidth(label), y + fm.getAscent() / 2 - 2);
@@ -136,10 +143,35 @@ public class DirectionHeatmapPanel extends JPanel {
         double elMin = elevationsDeg[0];
         double elMax = elevationsDeg[elevationsDeg.length - 1];
 
-        int x = margin + (int) ((azDeg - azMin) / (azMax - azMin) * plotWidth);
-        int y = margin + plotHeight - (int) ((elDeg - elMin) / (elMax - elMin) * plotHeight);
+        int x = valueToX(azDeg, azMin, azMax, margin, plotWidth);
+        int y = valueToY(elDeg, elMin, elMax, margin, plotHeight);
         g2.drawOval(x - 5, y - 5, 10, 10);
         g2.drawLine(x - 8, y, x + 8, y);
         g2.drawLine(x, y - 8, x, y + 8);
+    }
+
+    private int valueToX(double value, double min, double max, int margin, int plotWidth) {
+        return margin + (int) Math.round((value - min) / (max - min) * plotWidth);
+    }
+
+    private int valueToY(double value, double min, double max, int margin, int plotHeight) {
+        return margin + plotHeight - (int) Math.round((value - min) / (max - min) * plotHeight);
+    }
+
+    private double[] computeBounds(double[] coordinates) {
+        double[] bounds = new double[coordinates.length + 1];
+        if (coordinates.length == 1) {
+            bounds[0] = coordinates[0] - 0.5;
+            bounds[1] = coordinates[0] + 0.5;
+            return bounds;
+        }
+
+        bounds[0] = coordinates[0] - (coordinates[1] - coordinates[0]) / 2.0;
+        for (int i = 1; i < coordinates.length; i++) {
+            bounds[i] = (coordinates[i - 1] + coordinates[i]) / 2.0;
+        }
+        bounds[coordinates.length] = coordinates[coordinates.length - 1]
+                + (coordinates[coordinates.length - 1] - coordinates[coordinates.length - 2]) / 2.0;
+        return bounds;
     }
 }
